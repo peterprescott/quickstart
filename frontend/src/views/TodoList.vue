7558e9e7841d7b1b13fa38b5794313167f5009e1
Peter Prescott <prescott.peter@gmail.com>
2020-05-18 12:05:58 +0100

    front connected to back, but router now throws errors

---
+++ frontend/src/views/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<h1>My Todo App!</h1>
+		<TodoList apiURL='http://localhost:5000' />
+	</div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList.vue'
+
+export default {
+	components: {
+		TodoList
+	},
+
+	data () {
+		/* apiURL: '' */
+
+	},
+
+	methods: {
+		setAPI() {
+			/* find current URL */
+			console.log(window.location.pathname)
+			/* this.apiURL = 'http://localhost:5000' */
+
+		}, 
+	},
+
+	mounted() {
+		/* this.setAPI() */
+	},
+
+}
+</script>
